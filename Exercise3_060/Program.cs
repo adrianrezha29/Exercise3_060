0360d9d005848a09360dcaf13eb4372186c4ac2c
adrianrezha29 <adrianrezha29@gmail.com>
2022-11-25 16:52:42 +0700

    add case 1,2,3,4 to menampilkan data yang telah dimasukkan

--- Exercise3_060/Program.cs
+++ Exercise3_060/Program.cs
@@ -77,8 +77,49 @@ namespace Exercise3_060
                     char ch = Convert.ToChar(Console.ReadLine());
                     switch (ch)
                     {
-
+                        case '1':
+                            {
+                                obj.traverse();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.listEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Node prev, curr;
+                                prev = curr = null;
+                                Console.WriteLine("\nEnter the roll number of the student whose record is to be searched: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.Search(num, ref prev, ResolveEventArgs curr) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("\nRecord found");
+                                    Console.WriteLine("\nRoll number: " + curr.rollNumber);
+                                    Console.WriteLine("\mName: " + curr.name);
+                                }
+                            }
+                            break;
+                        case '3':
+                            {
+                                obj.firstNode();
+                            }
+                            break;
+                        case '4':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("Invalid option");
+                                break;
+                            }
                     }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString()); 
                 }
             }
         }
